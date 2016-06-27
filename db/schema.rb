3c91dad00ed8827f26302f83f7eaa2b14a21c95a
Cam <camlatimer@gmail.com>
2016-06-27 17:46:06 -0400

    adds info for seeding database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627001628) do
+ActiveRecord::Schema.define(version: 20160627191738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20160627001628) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.text     "details"
+    t.string   "sounds"
   end
 
   add_foreign_key "affiliations", "artists"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+data = JSON.parse(File.read("db/data.json"))
+
+
+Artist.destroy_all
+Affiliation.destroy_all
+Artist.create!(data)
+
+car = Artist.find_by(name: 'Mr. Carmack')
+falc = Artist.find_by(name: 'Falcons')
+gold = Artist.find_by(name: 'Goldlink')
+diplo = Artist.find_by(name: 'Diplo')
+skril = Artist.find_by(name: 'Skrillex')
+zhu = Artist.find_by(name: 'ZHU')
+ferg = Artist.find_by(name: 'ASAP Ferg')
+drake = Artist.find_by(name: 'Drake')
+chainz = Artist.find_by(name: '2 Chainz')
+kanye = Artist.find_by(name: 'Kanye West')
+jeezy = Artist.find_by(name: 'Jeezy')
+gj = Artist.find_by(name: 'Gent & Jawns')
+future = Artist.find_by(name: 'Future')
+calvin = Artist.find_by(name: 'Calvin Harris')
+rih = Artist.find_by(name: 'Rihanna')
+
+falc.affiliates << [gold, car]
+
+ferg.affiliates << [future]
+
+skril.affiliates << [diplo, ferg, zhu]
+
+diplo.affiliates << [gj, chainz]
+
+chainz.affiliates << [jeezy, kanye, drake]
+
+drake.affiliates << [jeezy, kanye, future]
