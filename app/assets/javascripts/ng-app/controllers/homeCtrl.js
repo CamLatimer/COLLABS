336a3d9216ae4104025fd63f33a96721b72eda84
Cam <camlatimer@gmail.com>
2016-07-09 12:32:30 -0400

    on click collaborators for given artist are shown and others get hidden; populates map with markers for collaborators locations

--- app/assets/javascripts/ng-app/controllers/homeCtrl.js
+++ app/assets/javascripts/ng-app/controllers/homeCtrl.js
@@ -7,8 +7,10 @@
 		function ($scope, $http) {
 
 				// scope variables
+				$scope.selectedIndex;
 				$scope.keywords;
 				$scope.artists;
+				$scope.locations = [];
 
 				// searches through all artists and brings back individual objects with
 				// // info and list of collaborators
@@ -24,31 +26,10 @@
 				}
 
 				// trigger ng-show to display the collaborators for each artist on click
-				$scope.showCollabs = function(context, index){
-					if(context.showing === true){
-						context.showing = false;
-					} else {
-						context.showing = true;
-					}
+				$scope.showCollabs = function(index){
 					var collabs = getCollabs(index);
-					var locations = getLocations(collabs);
-					getGeocodeRes(locations);
+					var artistLocations = getLocations(collabs);
 				}
-
-				/*  goal: display a marker on the map for each of an artist's collaborators
-        		problem: how to use google maps geocode api to grab long / lat and
-        		use that data with the googlemaps-ui directive to make markers
-        		approach:
-
-				* 		grab long and lat for each one of the collaborators of a selected artist
-							and put them into an array to use with googlemaps-ui markers directvie
-								grab the collaborators for a selected artist
-								grab the city and country of each collaborator
-								use the city and country in call to geocode api
-					  		grab geocode api response and parse results to get long / lat
-								put long / lat into an object
-								put object into an array
-        */
 
 				// grab the collaborators for a selected artist
 				function getCollabs(index){
@@ -57,32 +38,30 @@
 
 				// grab the city and country of each collaborator
 				function getLocations(collabArray){
-					var locations = [];
+					$scope.locations = [];
 					collabArray.forEach(function(collab){
 						var locationObj = {};
 						locationObj.name = collab.name;
-						locationObj.city = collab.city;
-						locationObj.country = collab.country;
-						locations.push(locationObj);
+						locationObj.id = collab.id;
+						// grab geocode api response and parse results to get long / lat
+						geoCode(collab.city, collab.country, function(res){
+							// put long / lat into an object
+								locationObj.latitude = res.data.results[0].geometry.location.lat;
+								locationObj.longitude = res.data.results[0].geometry.location.lng;
+						} )
+						// put object into an array
+						$scope.locations.push(locationObj);
 					})
-					return locations;
+					console.log($scope.locations);
+					return $scope.locations;
 				}
 
 				// use the city and country in call to geocode api
 				function geoCode(city, country, callback){
-					return $http.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + city + ",+" + country)
-							 .then(callback);
-				}
-
-				// grab geocode api response and parse results to get long / lat
-				function getGeocodeRes(locationsArray){
-					locationsArray.forEach(function(obj){
-						geoCode(obj.city, obj.country, function(res, obj){
-								obj.latitude = res.data.results[0].geometry.location.lat;
-								obj.longitude = res.data.results[0].geometry.location.lng;
-						});
-						console.log(obj);
-					})
+					return $http.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + city + ",+" + "country")
+							 .then(callback, function(error){
+								 console.log(error);
+							 });
 				}
 
 
